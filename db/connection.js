e1a21894a672d9836e05b6091b3f376dd0878bea
Amit Mandal <138322339+amit-mandal-kgce@users.noreply.github.com>
2023-12-22 20:04:10 +0530

    modify db

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
-
-mongoose
-  .connect(
-    "mongodb+srv://agencyapps:Amit12345Mandal@cluster0.nqes8i2.mongodb.net/?retryWrites=true&w=majority"
-  )
+MONGODB_CONNECT_URL ="mongodb+srv://agencyapps:Amit12345Mandal@cluster0.nqes8i2.mongodb.net/Cluster0?retryWrites=true&w=majority";
+mongoose.connect(MONGODB_CONNECT_URL)
   .then(() => console.log("Connect db"))
   .catch((e) => console.log("Error", e));
